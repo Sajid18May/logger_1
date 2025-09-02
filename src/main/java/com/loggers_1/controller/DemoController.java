22c0e2003dde16b99394044ed635e54887075327
Sajid18May <sajid1852002@gmail.com>
2025-09-02 12:43:03 +0530

    modified the controller

--- src/main/java/com/loggers_1/controller/DemoController.java
+++ src/main/java/com/loggers_1/controller/DemoController.java
@@ -3,7 +3,6 @@ package com.loggers_1.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.loggers_1.service.DemoService;
